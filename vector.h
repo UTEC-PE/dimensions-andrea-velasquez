0024687cf7d72b1275286cbac01c8980391423ce
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2018-09-15 21:03:11 -0500

    Implement vector constructor

--- vector.h
+++ vector.h
@@ -9,7 +9,7 @@ class Vector {
              
     private:
         T* data;
-        int dataSize;
+        int dataSize = 0;
 
         int dimensions;
         int* dimensionSizes;
@@ -18,9 +18,12 @@ class Vector {
         Vector() : data(nullptr) {};
              
         Vector(int dimensions, int* dimensionSizes) : dimensions(dimensions), dimensionSizes(dimensionSizes) {
-            // TODO
+          for (int i=0; i<dimensions; i++) dataSize*=dimensionSizes[i];
+          data = new T [dataSize];
         }
              
+        Vector(int* dimensionSizes) :
+        Vector(sizeof(dimensionSizes)/sizeof(dimensionSizes[0]), dimensionSizes) {}
         void set(T datum, int* coordinates); // TODO
              
         T get(int* coordinates); // TODO
