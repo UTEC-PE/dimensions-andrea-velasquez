a6f4006975e1f1c9dd8f7fa5163173e78bcffecf
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2018-09-15 21:06:30 -0500

    Implement dimensions operator

--- main.cpp
+++ main.cpp
@@ -6,10 +6,15 @@
 using namespace std;
 
 template <typename T>
-struct Dimensions {      
-    int operator()(int* coordinates, int* sizes, int dimensions) {
-        // TODO
+struct Dimensions {
+  int operator()(int* coordinates, int* sizes, int dimensions) {
+    int ans = coordinates[dimensions-1];
+    for (int i=dimensions-2; i>-1; i--){
+      ans *=sizes[i];
+      ans+=coordinates[i];
     }
+    return ans;
+  }
 };
 
 struct Integer {
